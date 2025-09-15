4b232305b41a4e3095af4d7f49d248e25de1e78b
CodeExplorerX <codeexplorerx@proton.me>
2025-09-15 13:21:26 +0200

    migrate files

--- songbook/frontend/src/App.jsx
+++ songbook/frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes,  } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import MainSite from './components/MainSite';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App(){
+  return(
+    <div className='App'>
+      <Router>
+        <div>
+        {/* nav links */}
+          <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/main" element={<MainSite />} />
+          </Routes>
+          {/* <Footer /> */}
+        </div>
+      </Router>
+    </div>
+  );
 }
-
-export default App
+export default App;
--- songbook/frontend/src/main.jsx
+++ songbook/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import reportWebVitals from './reportWebVitals';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -8,3 +9,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+reportWebVitals();
